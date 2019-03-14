c7e91dd3e632f17f5d7e438cbd5b4d5215d8c9de
jonahtash <jtash@umd.edu>
2019-03-14 02:47:56 -0400

    Update main.py

--- piServer/main.py
+++ piServer/main.py
@@ -1,8 +1,9 @@
 from flask import Flask, request
 app = Flask(__name__)
 @app.route('/', methods=['POST'])
-def process():
+def process(data):
     print("TODO")
+    return "FIP"
 def result():
     username = process(request.form['data']);
     return username
